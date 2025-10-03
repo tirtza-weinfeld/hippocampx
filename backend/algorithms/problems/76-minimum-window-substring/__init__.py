831db3439bb78220f03087ae96567bcd19589427
tz-i-git <tzu.22@icloud.com>
2025-10-03 14:49:26 +0300

    fix build

--- backend/algorithms/problems/76-minimum-window-substring/__init__.py
+++ backend/algorithms/problems/76-minimum-window-substring/__init__.py
@@ -1,7 +1,10 @@
 """
-Title:
-Definition:
-Leetcode: https://leetcode.com/problems/
-Difficulty:
-Topics:
+Title:Minimum Window Substring
+Definition:Given two strings `s` and `t` of lengths `m` and `n` respectively, \
+    return the minimum window substring of `s` such that every character in `t` (including duplicates) \
+    is included in the window. If no such window exists, return an empty string `""`,
+    *[19!]solve it in linear time complexity O(m + n)*
+Leetcode: https://leetcode.com/problems/minimum-window-substring
+Difficulty:hard
+Topics:[sliding-window]
 """
