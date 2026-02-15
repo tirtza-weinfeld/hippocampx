bbebcacbee0180d42acd95e7f967ec6e2df892e4
tz-i-git <tzu.22@icloud.com>
2026-02-15 19:55:01 +0200

    fix(build): remove old mdx files to make the build smaller

--- app/notes/tailwind/layout.tsx
+++
@@ -1,9 +0,0 @@
-// import '@/styles/components/tailwind-utilities-demo.css'
-
-export default function TailwindNotesLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return children
-}
--- app/notes/test/page.tsx
+++ app/notes/test/page.tsx
@@ -1,9 +1,9 @@
 
-import ReverseNodesInKGroup  from '@/components/problems/tutorials/./25-reverse-nodes-in-k-group.mdx'
-import Countsmaller  from '@/components/problems/tutorials/./315-count-of-smaller-numbers-after-self.mdx'
-import LRU from '@/components/problems/tutorials/146-lru-cache.mdx'
-import Word from '@/components/problems/tutorials/79-word-search.mdx'
-import Word2 from '@/components/problems/tutorials/212-word-search-ii.mdx'
+// import ReverseNodesInKGroup  from '@/components/problems/tutorials/./25-reverse-nodes-in-k-group.mdx'
+// import Countsmaller  from '@/components/problems/tutorials/./315-count-of-smaller-numbers-after-self.mdx'
+// import LRU from '@/components/problems/tutorials/146-lru-cache.mdx'
+// import Word from '@/components/problems/tutorials/79-word-search.mdx'
+// import Word2 from '@/components/problems/tutorials/212-word-search-ii.mdx'
 export default function TestPage() {
  
   
@@ -13,7 +13,7 @@ export default function TestPage() {
      {/* <Countsmaller /> */}
      {/* <LRU/> */}
      {/* <Word/> */}
-     <Word2/>
+     {/* <Word2/> */}
     </div>
   )
 }
