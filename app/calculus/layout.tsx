712fa17d78af81befcfacd97b0b0a18441a1a25e
tz-i-git <tzu.22@icloud.com>
2026-02-17 12:00:26 +0200

    refactor(app): remove unused route directories

--- app/calculus/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="mx-auto max-w-7xl ">
-            {children}
-        </div>
-    );
-}
